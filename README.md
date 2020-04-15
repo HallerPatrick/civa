29b39d0f748ccb1c96eab4fd51b0c5a6e92b42f1
Patrick Haller <patrickhaller40@googlemail.com>
2020-04-16 00:56:39 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Todo
+
+- [ ] Make Signal Handling proper (use rustyline or signal_hook?)
+- [ ] Read more
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use ctrlc;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+// use signal_hook::iterator::Signals;
+// use signal_hook::SIGINT;
+use std::env;
+use std::io;
+use std::path::Path;
+use std::process::Command;
+use std::thread;
+
+fn main() {
+    handle_signals();
+
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        match rl.readline("> ") {
+            Ok(line) => {
+                let commands: Vec<&str> = line.split(" ").collect::<Vec<&str>>();
+
+                // Check first for predefined commands
+                if commands[0] == "cd" {
+                    match cd(commands[1]) {
+                        Ok(()) => {}
+                        Err(err) => println!("{}", err),
+                    }
+                } else {
+                    let child = Command::new(commands[0]).args(&commands[1..]).spawn();
+
+                    match child {
+                        Ok(mut c) => match c.wait() {
+                            Ok(exit_status) => println!("Exist Status: {}", exit_status),
+                            Err(err) => {
+                                panic!("Could not wait for child process to finish: {}", err);
+                            }
+                        },
+                        Err(err) => println!("Error: {}", err),
+                    }
+                }
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                eprintln!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+}
+
+fn handle_signals() {
+    // match Signals::new(&[SIGINT]) {
+    //     Ok(signals) => {
+    //         thread::spawn(move || {
+    //             for sig in signals.forever() {
+    //                 println!("Received signal {:?}", sig);
+    //             }
+    //         });
+    //     }
+    //     Err(err) => println!("Could not init signal hooks, Error: {}", err),
+    // };
+    ctrlc::set_handler(move || {
+        println!("received Ctrl+C!");
+    })
+    .expect("Could not init signal hooks.")
+}
+
+fn cd(path: &str) -> io::Result<()> {
+    env::set_current_dir(Path::new(&path))
+}
