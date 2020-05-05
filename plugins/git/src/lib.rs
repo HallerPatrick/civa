119ea3f82592654b55e791d42214e72df5d7e8c1
Patrick Haller <patrickhaller40@googlemail.com>
2020-05-05 13:47:18 +0200

    "test"

--- plugins/git/src/lib.rs
+++ plugins/git/src/lib.rs
@@ -1,5 +1,5 @@
-use std::process::Command;
 use std::process::Output;
+use std::process::{Command, Stdio};
 
 pub struct GitCli {}
 
@@ -13,20 +13,23 @@ impl GitCli {
         String::from_utf8(output.stdout).expect("Found invalid UTF-8")
     }
 
-    pub fn no_upstream_commits() -> usize {
-        let output: Output = Command::new("git")
-            .args(&["git", "cherry"])
+    pub fn no_upstream_commits<'a>() -> String {
+        let git_call = Command::new("git")
+            .args(&["cherry"])
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+
+        let wc = Command::new("wc")
+            .args(&["-l"])
+            .stdin(git_call.stdout.unwrap())
             .output()
             .unwrap();
 
-        let output = String::from_utf8(output.stdout).expect("Found invalid UTF-8");
-        println!("{:?}", output);
-
-        if output == "" {
-            0
-        } else {
-            output.split("\n").count()
-        }
+        String::from_utf8(wc.stdout)
+            .expect("Found invalid UTF-8")
+            .trim()
+            .to_string()
     }
 
     pub fn compose_git_component() -> String {
@@ -34,9 +37,9 @@ impl GitCli {
 
         comps.push(GitCli::get_current_branch().trim_end().to_string());
 
-        match GitCli::no_upstream_commits() {
-            0 => {}
-            num => comps.push(format!(" ⇡{}", num)),
+        let us = GitCli::no_upstream_commits();
+        if us != String::from("0") {
+            comps.push(format!(" ⇡{}", us));
         }
 
         comps.join("")
@@ -62,6 +65,6 @@ mod tests {
     fn it_works1() {
         let res = GitCli::no_upstream_commits();
 
-        assert_eq!(res, 0);
+        assert_eq!(res, String::from("1"));
     }
 }
